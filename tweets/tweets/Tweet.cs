e5538dfb507107598e4014dcaca615273ceb0035
Sipos Miklos <commit@siposm.hu>
2022-03-17 20:09:04 +0100

    entity classes created

---
+++ tweets/tweets/Tweet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tweets
+{
+    // https://docs.microsoft.com/en-us/ef/core/modeling/relationships?tabs=fluent-api%2Cfluent-api-simple-key%2Csimple-key
+
+    [Table("Tweets")]
+    public class Tweet
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public bool Flagged { get; set; }
+        public int Year { get; set; }
+
+        //public virtual int UserId { get; set; }
+        public virtual User User { get; set; }
+
+        public override string ToString()
+        {
+            return $"[{Id}] | {Year} - {Flagged} - {Content}";
+        }
+    }
+}
---
+++ tweets/tweets/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tweets
+{
+    // https://docs.microsoft.com/en-us/ef/core/modeling/relationships?tabs=fluent-api%2Cfluent-api-simple-key%2Csimple-key
+
+    [Table("Users")]
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+        public virtual ICollection<Tweet> Tweets { get; set; }
+
+        public User()
+        {
+            this.Tweets = new HashSet<Tweet>();
+        }
+
+        public override string ToString()
+        {
+            return $"[{Id}] | {UserName} - {UserEmail}";
+        }
+    }
+}
