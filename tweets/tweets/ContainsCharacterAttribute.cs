122b9b4198d582b4ed9ea1eaec358be0d17529a0
Sipos Miklos <commit@siposm.hu>
2022-03-17 21:14:51 +0100

    attribute class added

---
+++ tweets/tweets/ContainsCharacterAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tweets
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class ContainsCharacterAttribute : Attribute
+    {
+        public string CharactersToBeChecked { get; set; } // could be char[] as well...
+        public ContainsCharacterAttribute(string charactersToBeChecked)
+        {
+            this.CharactersToBeChecked = charactersToBeChecked;
+        }
+    }
+}
