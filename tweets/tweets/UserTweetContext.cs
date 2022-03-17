60468b89ead5519d924acff45b1f760eeac547ca
Sipos Miklos <commit@siposm.hu>
2022-03-17 20:09:10 +0100

    db context class created

---
+++ tweets/tweets/UserTweetContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tweets
+{
+    public class UserTweetContext : DbContext
+    {
+        public virtual DbSet<Tweet> Tweets { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+
+        public UserTweetContext()
+        {
+            this.Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder ob)
+        {
+            if(!ob.IsConfigured)
+            {
+                ob.UseLazyLoadingProxies();
+                ob.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\UserTweets.mdf;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<Tweet>()
+                .HasOne(x => x.User)
+                .WithMany(x => x.Tweets);
+
+            // https://docs.microsoft.com/en-us/ef/core/modeling/relationships?tabs=fluent-api%2Cfluent-api-simple-key%2Csimple-key
+        }
+    }
+}
