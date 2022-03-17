87b2634f40bb943ae6ce8bf2f163382176079b7b
Sipos Miklos <commit@siposm.hu>
2022-03-17 20:09:15 +0100

    test in main added

--- tweets/tweets/Program.cs
+++ tweets/tweets/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace tweets
 {
@@ -6,7 +7,31 @@ namespace tweets
     {
         static void Main(string[] args)
         {
-            // Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\UserTweets.mdf;Integrated Security=True
+            UserTweetContext db = new UserTweetContext();
+            
+            User u = new User();
+            u.UserName = "test_user";
+            u.UserEmail = "test@mail.com";
+            u.Tweets = new List<Tweet>();
+            u.Tweets.Add(new Tweet()
+            {
+                Content = "asdasdasd",
+                Flagged = true,
+                Year = 2022
+            });
+
+            db.Users.Add(u);
+
+            db.SaveChanges();
+
+            foreach (var item in db.Users)
+            {
+                Console.WriteLine(item);
+                foreach (var tw in item.Tweets)
+                {
+                    Console.WriteLine(tw);
+                }
+            }
         }
     }
 }
