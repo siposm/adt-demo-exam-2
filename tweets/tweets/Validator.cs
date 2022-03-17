fef270c9e85a57a351be13fdf27d1a0bc592933f
Sipos Miklos <commit@siposm.hu>
2022-03-17 21:15:06 +0100

    validator logic created

---
+++ tweets/tweets/Validator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tweets
+{
+    public class Validator
+    {
+        public static bool UserEmailValidator<T>(string propName, T obj)
+        {
+            PropertyInfo property = obj.GetType().GetProperty(propName);
+
+            ContainsCharacterAttribute cca = property.GetCustomAttributes(typeof(ContainsCharacterAttribute), false).SingleOrDefault() as ContainsCharacterAttribute;
+
+            string propertyContent = property.GetValue(obj).ToString();
+
+            foreach (char character in cca.CharactersToBeChecked)
+            {
+                if (!propertyContent.Contains(character))
+                    return false;
+            }
+            return true;
+        }
+    }
+}
